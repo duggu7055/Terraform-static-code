f82280a32d701680662a68dc3c6fee0958db7e33
Durgesh Sharma <111552304+duggu7055@users.noreply.github.com>
2025-07-09 23:53:11 +0530

    Update backend.tf

--- Network-skelton/VPC/backend.tf
+++ Network-skelton/VPC/backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
     bucket         = "tfstate-p14-demo"
-    key            = "env/dev/network-skeleton/vpc/terraform.tfstate"
-    region         = "ap-northeast-1"
+    key            = "network-skeleton/vpc/terraform.tfstate"
+    region         = "ap-south-1"
     dynamodb_table = "sprint4-statelock"
     encrypt        = true
   }
