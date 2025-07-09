965f08e826b6c870a9f4081a4b052218c66ef81c
Durgesh Sharma <111552304+duggu7055@users.noreply.github.com>
2025-07-09 23:43:46 +0530

    Create backend.tf

---
+++ Network-skelton/VPC/backend.tf
@@ -0,0 +1,9 @@
+terraform {
+  backend "s3" {
+    bucket         = "tfstate-p14-sprint4"
+    key            = "env/dev/network-skeleton/vpc/terraform.tfstate"
+    region         = "ap-northeast-1"
+    dynamodb_table = "sprint4-statelock"
+    encrypt        = true
+  }
+}
