6de67b97ceed61e520a21e633a818ccfb4a57245
Durgesh Sharma <111552304+duggu7055@users.noreply.github.com>
2025-07-11 02:49:47 +0530

    Delete Network-skelton/VPC/backend.tf

--- Network-skelton/VPC/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "tfstate-p14-demo"
-    key            = "network-skeleton/vpc/terraform.tfstate"
-    region         = "ap-south-1"
-    dynamodb_table = "sprint4-statelock"
-    encrypt        = true
-  }
-}
